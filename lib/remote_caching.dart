e5cc1fbaa009b3c9b2de5054cef48e47a3e6c5db
Elia Tolin <elia.tolin@auroradigital.it>
2025-07-08 23:31:41 +0200

    dev: fix dart format

--- lib/remote_caching.dart
+++ lib/remote_caching.dart
@@ -29,7 +29,6 @@ library remote_caching;
 
 import 'package:remote_caching/remote_caching.dart';
 
-
 export 'src/common/get_in_memory_database.dart' show getInMemoryDatabasePath;
 export 'src/models/caching_stats.dart' show CachingStats;
 export 'src/remote_caching_impl.dart' show RemoteCaching;
